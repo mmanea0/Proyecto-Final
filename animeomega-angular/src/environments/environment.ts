2dc6d891f9292bd63b659c63e2a94e71a97e109a
manea0 <mmanea01@iescastelar.com>
2024-06-05 23:54:54 +0200

    update env

--- animeomega-angular/src/environments/environment.ts
+++ animeomega-angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlapi: 'http://localhost/api',
-  // urlapi: 'http://mysterunicorn.duckdns.org:2284/api',
+  // urlapi: 'http://localhost/api',
+  urlapi: 'http://mysterunicorn.duckdns.org:2284/api',
 };
